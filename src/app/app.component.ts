35a1b5f189252f3e350c1de4cbeb41b52a0a4eab
Jason Jean <jjean@forbes.com>
2017-12-17 01:53:17 -0500

    Add working Server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'fbs-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fbs';
+
+  ngOnInit() {
+    console.log(`Let's get this party started`);
+  }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,36 @@
+import { enableProdMode } from '@angular/core';
+import { ngExpressEngine } from '@nguniversal/express-engine';
+
+import 'zone.js/dist/zone-node';
+
+import * as path from 'path';
+import * as express from 'express';
 
 const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('../dist-server/main.bundle');
 
+const server = express();
 
+const root = path.resolve(__dirname, '..');
+const distPath = path.resolve(root, 'dist');
+const indexPath = path.resolve(distPath, 'index.html');
+
+enableProdMode();
+
+server.use(express.static(distPath, {
+  index: false
+}));
+
+server.engine('html', ngExpressEngine({
+  bootstrap: AppServerModuleNgFactory
+}));
+
+server.get('*', (req, res) => {
+  res.render(indexPath, {
+    req,
+    res
+  });
+});
+
+server.listen(4300, (err) => {
+  console.log('Server has started: http://localhost:4300');
+});
