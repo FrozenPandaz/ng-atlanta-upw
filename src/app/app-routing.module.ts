690580c6c5270a66c0ddf7a790a84afe531e89ac
Jason Jean <jjean@forbes.com>
2017-12-17 15:22:40 -0500

    Add Profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'profile',
+    loadChildren: './profile/profile.module#ProfileModule'
+  },
+  {
     path: '',
     pathMatch: 'full',
     loadChildren: './lander/lander.module#LanderModule'
---
+++ src/app/profile/profile.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProfileRoutingModule } from './profile-routing.module';
+import { ProfileComponent } from './profile.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ProfileRoutingModule
+  ],
+  declarations: [ProfileComponent]
+})
+export class ProfileModule { }
